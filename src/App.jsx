cae04b782ee951f58a0c49d3ad7021a0414f7123
Juliana <aragaopintojuli@gmail.com>
2023-02-19 20:16:59 -0300

    finsh ?

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import Card from './components/Card';
 import { specialCards, number } from './utils/cards';
 import Logo from './assets/logo.svg';
+import GH from './assets/gh.svg';
 
 function App() {
   const [color, setColor] = useState('--red');
@@ -12,43 +13,53 @@ function App() {
     <>
       <header>
         <img src={Logo} alt="logo" />
+        <a
+          href="https://github.com/julipinto/uno-cards"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={GH} alt="github" />
+        </a>
       </header>
       <main style={{ '--card-color': `var(${color})` }}>
         <Card card={card} />
         <aside>
-          <span>Colors:</span>
-          <div className="button_group">
-            {['--red', '--yellow', '--blue', '--green'].map((color) => (
-              <button
-                id={color}
-                style={{ '--button-color': `var(${color})` }}
-                onClick={() => setColor(color)}
-              />
-            ))}
+          <div>
+            <span>Colors:</span>
+            <div className="button_group">
+              {['--red', '--yellow', '--blue', '--green'].map((color) => (
+                <button
+                  id={color}
+                  style={{ '--button-color': `var(${color})` }}
+                  onClick={() => setColor(color)}
+                />
+              ))}
+            </div>
           </div>
-        </aside>
-        <aside>
-          <span>Cards:</span>
-          <div className="button_group">
-            {['0', '1', '2', '3', '4'].map((id) => (
-              <button id={id} onClick={() => setId(id)}>
-                {id}
-              </button>
-            ))}
-          </div>
-          <div className="button_group">
-            {['5', '6', '7', '8', '9'].map((id) => (
-              <button id={id} onClick={() => setId(id)}>
-                {id}
-              </button>
-            ))}
-          </div>
-          <div className="button_group">
-            {['+4', '+2', '↻', '⦸'].map((id) => (
-              <button id={id} onClick={() => setId(id)}>
-                {id}
-              </button>
-            ))}
+          <div>
+            <span>Cards:</span>
+            <div className="button_group">
+              {[
+                '0',
+                '1',
+                '2',
+                '3',
+                '4',
+                '5',
+                '6',
+                '7',
+                '8',
+                '9',
+                '+4',
+                '+2',
+                '↻',
+                '⦸',
+              ].map((id) => (
+                <button id={id} onClick={() => setId(id)}>
+                  {id}
+                </button>
+              ))}
+            </div>
           </div>
         </aside>
       </main>
--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,16 +1,22 @@
+import { useState } from 'react';
 import { useSpring, animated, to } from 'react-spring';
 import './style.css';
 
+// on hover moviment
 const calc = (x, y) => [
-  -((0 + 300 / 2 - y) / (300 / 2)) * 10,
-  ((0 + 200 / 2 - x) / (200 / 2)) * 10,
-  1.1,
+  -(y - window.innerHeight / 2) / 10,
+  (x - window.innerWidth / 2) / 5,
+  1,
 ];
 
-const trans = (x, y, s) =>
-  `perspective(1000px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`;
+// on hover transform
 
 function Card({ card }) {
+  const [flipped, setFlipped] = useState(true);
+
+  const trans = (x, y, s) =>
+    `perspective(1000px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`;
+
   const [props, set] = useSpring(
     () => ({
       xys: [0, 0, 1],
@@ -24,17 +30,26 @@ function Card({ card }) {
     ...(card.color && { backgroundColor: `var(${card.color})` }),
   };
 
+  console.log(style);
+
   return (
-    <animated.div
-      className="card"
-      onMouseMove={({ clientX: x, clientY: y }) => set({ xys: calc(x, y) })}
-      onMouseLeave={() => set({ xys: [0, 0, 1] })}
-      style={style}
-    >
-      <span className="top_sym">{card.top_bot_sym && card.top_bot_sym()}</span>
-      <span className="mid_sym">{card.mid_sym && card.mid_sym()}</span>
-      <span className="bot_sym">{card.top_bot_sym && card.top_bot_sym()}</span>
-    </animated.div>
+    <>
+      <animated.div
+        className={`card ${flipped ? 'flip' : ''}`}
+        onMouseMove={({ clientX: x, clientY: y }) => set({ xys: calc(x, y) })}
+        onMouseLeave={() => set({ xys: [0, 0, 1] })}
+        style={style}
+        onClick={() => setFlipped(!flipped)}
+      >
+        <span className="top_sym">
+          {card.top_bot_sym && card.top_bot_sym()}
+        </span>
+        <span className="mid_sym">{card.mid_sym && card.mid_sym()}</span>
+        <span className="bot_sym">
+          {card.top_bot_sym && card.top_bot_sym()}
+        </span>
+      </animated.div>
+    </>
   );
 }
 
